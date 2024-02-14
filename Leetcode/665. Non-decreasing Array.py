9482c892ad7ea570e12864f288ed3b0f602fe627
Nader Mamdouh <nadermamdouh20@gmail.com>
2024-02-15 00:44:17 +0200

    Create 665. Non-decreasing Array.py

---
+++ Leetcode/665. Non-decreasing Array.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def checkPossibility(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        modified = False  
+        for i in range(1, len(nums)):
+            if nums[i] < nums[i - 1]:  
+                if modified:
+                    return False  
+                if i == 1 or nums[i] >= nums[i - 2]:
+                    nums[i - 1] = nums[i]  
+                else:
+                    nums[i] = nums[i - 1]  
+                modified = True
+        return True
+        
